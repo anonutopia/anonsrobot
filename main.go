1a1648a1deaaf1285b281b11abf83e8e0233c908
Pragmatic <cryptopragmatic@protonmail.com>
2021-04-08 15:35:47 +0200

    deploy script

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	"gopkg.in/tucnak/telebot.v2"
+	"gorm.io/gorm"
 )
 
 var conf *Config
 
 var bot *telebot.Bot
+
+var db *gorm.DB
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -16,6 +19,8 @@ func main() {
 	conf = initConfig()
 
 	bot = initTelegramBot()
+
+	db = initDb()
 
 	logTelegram("AnonsRobot daemon successfully started. 🚀")
 	log.Println("AnonsRobot daemon successfully started. 🚀")
