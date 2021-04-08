6d1387ce5c780627860aaba1b27bb7ddfe8a2533
Pragmatic <cryptopragmatic@protonmail.com>
2021-04-08 15:12:06 +0200

    db work

---
+++ const.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	// TelAnonOps group for error logging
+	TelAnonOps = -1001213539865
+
+	// TelAnonTeam group for team messages
+	TelAnonTeam = -1001280228955
+)
---
+++ db.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func initDb() *gorm.DB {
+	var db *gorm.DB
+	var err error
+	dbconf := gorm.Config{}
+
+	if conf.Debug {
+		dbconf.Logger = logger.Default.LogMode(logger.Info)
+	} else {
+		dbconf.Logger = logger.Default.LogMode(logger.Error)
+	}
+
+	if conf.Dev {
+		db, err = gorm.Open(sqlite.Open("anonsrobot.db"), &dbconf)
+	} else {
+		db, err = gorm.Open(postgres.Open(conf.PostgreSQL), &dbconf)
+	}
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	return db
+}
